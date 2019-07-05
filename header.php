0f89a6a6f512383b984d620a08e85bd524f6dbb3
Kirsten Malcaba <kmalcaba@live.com>
2019-07-05 21:44:34 +0800

    changed Budget to Expense

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
                     <div class="sidebar-brand-icon rotate-n-15">
                         <i class="fas fa-laugh-wink"></i>
                     </div>
-                    <div class="sidebar-brand-text mx-3">Budget Tracker</div>
+                    <div class="sidebar-brand-text mx-3">Expense Tracker</div>
                 </a>
 
                 <!-- Divider -->
