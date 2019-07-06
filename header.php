da2032a1de28597dc6a28f2e814a9d34e0fc529b
Renz Albandia <renzalbandia@outlook.com>
2019-07-06 09:23:01 +0800

    Update header.php

--- header.php
+++ header.php
@@ -41,7 +41,7 @@ if($_SESSION['login'] == FALSE){
             <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
                 <!-- Sidebar - Brand -->
-                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="Dashboard.php">
                     <div class="sidebar-brand-icon rotate-n-15">
                         <i class="fas fa-laugh-wink"></i>
                     </div>
