5f0bfd88c3f85a729141d3770b0cdfb7e345f9b7
Bryan Joshua B. Edejer <bryan_edejer@outlook.com>
2019-07-06 17:11:26 +0800

    Minor format

--- Dashboard.php
+++ Dashboard.php
@@ -254,6 +254,23 @@ $monthly_income_list = json_encode(array($january_income, $feb_income, $march_in
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-4 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Income</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">₱<?php echo $income ?></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-4 col-md-6 mb-4">
             <div class="card border-left-danger shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -265,23 +282,6 @@ $monthly_income_list = json_encode(array($january_income, $feb_income, $march_in
                             <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
                         </div>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-4 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Income</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">₱<?php echo $income ?></div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
                     </div>
                 </div>
             </div>
